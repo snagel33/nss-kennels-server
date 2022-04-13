19a25c608101dea9df5b0e41921c6411f33a28e8
Steve Nagel <stevencnagel@gmail.com>
2022-04-12 21:25:14 -0500

    chapter 4 locations, employees, customers add func added

--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,9 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from views import get_all_animals, get_single_animal, create_animal
-from views import get_all_locations, get_single_location
-from views import get_all_employees, get_single_employee
-from views import get_all_customers, get_single_customer
+from views import get_all_locations, get_single_location, create_location
+from views import get_all_employees, get_single_employee, create_employee
+from views import get_all_customers, get_single_customer, create_customer
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -119,6 +119,9 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         # Initialize new animal
         new_animal = None
+        new_location = None
+        new_employee = None
+        new_customer = None
 
         # Add a new animal to the list. Don't worry about
         # the orange squiggle, you'll define the create_animal
@@ -128,6 +131,21 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         # Encode the new animal and send in response
         self.wfile.write(f"{new_animal}".encode())
+        
+        if resource == "locations":
+            new_location = create_location(post_body)
+            
+        self.wfile.write(f"{new_location}".encode())
+        
+        if resource == "employees":
+            new_employee = create_employee(post_body)
+            
+        self.wfile.write(f"{new_employee}".encode())
+        
+        if resource == "customers":
+            new_customer = create_customer(post_body)
+            
+        self.wfile.write(f"{new_customer}".encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
