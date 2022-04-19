ba8777f196061b524e86f8aa4a5f2145c994799b
Steve Nagel <stevencnagel@gmail.com>
2022-04-18 19:43:39 -0500

    Ch7 complete

---
+++ models/location.py
@@ -0,0 +1,7 @@
+class Location():
+    
+    def __inint__(self, id, name, address, status):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.status = status
